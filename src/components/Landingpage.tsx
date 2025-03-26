dbd8c1f8bc0daa49276e4a6930d1549fae3eaea2
JPromi <jonasmpr05@gmail.com>
2025-03-26 14:59:59 +0100

    add mobile friendly; change text

--- src/components/Landingpage.tsx
+++ src/components/Landingpage.tsx
@@ -1,19 +1,31 @@
 import './Landingpage.scss'
 import openVpnLogo from '../assets/openvpn.svg'
+import { useState } from 'react';
 
 export default function Landingpage() {
+    const [tankYou, setTankYou] = useState(false);
+
     return (
         <div className='landingpage'>
             <div className="container">
                 <div className="left">
                     <img src={openVpnLogo} alt='OpenVPN' />
                 </div>
+                <>
+                </>
                 <div className="right">
                     <h1>FF Perchtoldsdorf VPN Server</h1>
-                    <p>Mit unserer VPN-Verbindung erhältst du Zugriff auf das Netzwerk der FF Perchtoldsdorf.</p>
+                    {tankYou
+                        ? <p>Komme wieder zurück wenn du dich mit der VPN verbunden hast!</p>
+                        : <p>Willkommen auf unserem VPN-Server. Hier kannst du dir das Profil für die VPN-Verbindung herunterladen.</p>
+                    }
                     <div className="btns">
-                        <a className='orange' href='https://openvpn.net/client/'>VPN Client</a>
-                        <a href='/client.ovpn' download>Profil</a>
+                        <a href='https://openvpn.net/client/' className='orange'>
+                        {tankYou ? 'VPN Client noch nicht installiert?' : 'OpenVPN Client'}
+                        </a>
+                        {!tankYou && (
+                            <a href='/client.ovpn' download onClick={() => setTankYou(true)}>VPN Profil</a>
+                        )}
                     </div>
                 </div>
             </div>
