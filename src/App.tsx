52401cc78c09838c81dbe54620896722b182331c
JPromi <jonasmpr05@gmail.com>
2025-03-26 15:01:45 +0100

    remove old code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.scss'
 import Landingpage from './components/Landingpage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Landingpage />
   )
